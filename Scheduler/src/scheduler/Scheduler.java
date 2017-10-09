bb90e5e35214c56897c44c9c502f31d01be649b4
Mehtavarun <mehtavarun96@rediffmail.com>
2017-10-09 21:52:46 +0530

    DB adding data

--- Scheduler/src/scheduler/Scheduler.java
+++ Scheduler/src/scheduler/Scheduler.java
@@ -1,13 +1,32 @@
 package scheduler;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Scheduler {
 
     public static void main(String[] args) {
         
         DBHandling db = new DBHandling();
-        //db.newUser(1, "sanu", "kumar2010", "sanu");
-        //db.deleteUser(1);
-        //db.addASubject(1, 1, "Maths", 0, "", "");
+        
+//        db.newUser(1, "reese", "reesejohn", "reese");
+//        db.newUser(4, "sanu", "kumar2010", "sanu");
+//        db.deleteUser(4);
+//        db.addASubject(1, 1, "Maths",0, "3");
+//        db.DeleteASubject(4, 2, "Maths");
+        try {
+            db.scheduleSubjectsTime(1);
+        } catch (Exception ex) {
+            Logger.getLogger(Scheduler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//         
+           try{
+               db.st.close();
+               db.co.close();
+           } catch (Exception e){
+               Logger.getLogger(DBHandling.class.getName()).log(Level.SEVERE, null, e);
+           }
+            
     }
     
 }
